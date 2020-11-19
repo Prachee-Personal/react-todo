fadd29dbe376ad7d0de1f62bf187bb000f4619a0
unknown <pracheesingh07@gmail.com>
2020-11-19 12:54:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+// import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <TodoList/>
+            </div>
+        )
+    }
+}
+
+export default App
+
+
